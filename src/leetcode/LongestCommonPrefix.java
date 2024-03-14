54edf6fdb028e44f800e3d71ce1a8a801e5aa21b
sadik <shadikmgtbu@gmail.com>
2024-03-14 21:20:53 +0600

    problem solving on longest common prefix

---
+++ src/leetcode/LongestCommonPrefix.java
@@ -0,0 +1,21 @@
+package src.leetcode;
+
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefix();
+        String[] arr = {"flower", "flow", "fly"};
+        String commonPrefix = longestCommonPrefix.longestCommonPrefix(arr);
+        System.out.println(commonPrefix);
+    }
+    String longestCommonPrefix(String[] strs){
+        if (strs.length == 0)
+            return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++){
+            while (strs[i].indexOf(prefix) != 0){
+                prefix = prefix.substring(0,prefix.length() - 1);
+            }
+        }
+        return prefix;
+    }
+}
